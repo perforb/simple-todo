24e681750fa8ca0a628e58516ca4ad984b8d8bce
perforb <dev.perfumed.garden@gmail.com>
2018-05-30 18:09:36 +0900

    Update application class

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,12 +1,18 @@
 package com.example.todo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TodoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TodoApplication.class, args);
+    SpringApplicationBuilder builder = new SpringApplicationBuilder(TodoApplication.class);
+    builder
+      .bannerMode(Mode.CONSOLE)
+      .web(WebApplicationType.SERVLET)
+      .run(args);
   }
 }
