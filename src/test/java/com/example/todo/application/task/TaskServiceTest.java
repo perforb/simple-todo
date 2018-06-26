ac2c62e831b842fa05db3758227df4c721b69c6c
perforb <dev.perfumed.garden@gmail.com>
2018-06-26 12:37:57 +0900

    Add a test class

---
+++ src/test/java/com/example/todo/application/task/TaskServiceTest.java
@@ -0,0 +1,85 @@
+package com.example.todo.application.task;
+
+import com.example.todo.domain.NotFoundException;
+import com.example.todo.domain.task.Task;
+import com.example.todo.domain.task.TaskRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+class TaskServiceTest {
+
+  @Mock
+  private TaskRepository taskRepository;
+
+  @InjectMocks
+  private TaskService underTest;
+
+  @Test
+  void list() {
+  }
+
+  @Test
+  void findById() {
+    when(taskRepository.findById(anyInt())).thenReturn(
+      Optional.of(new Task(
+        1,
+        "task1",
+        false,
+        LocalDateTime.now(ZoneId.of("Asia/Tokyo"))
+      ))
+    );
+
+    Optional<Task> task = taskRepository.findById(1);
+    Assertions.assertTrue(task.isPresent());
+    task.ifPresent(t -> assertAll("equal properties",
+      () -> assertEquals(Integer.valueOf(1), t.getId()),
+      () -> assertEquals("task1", t.getTitle()),
+      () -> assertFalse(t.isDone())
+    ));
+  }
+
+  @Test
+  void findByIdIfNotFound() {
+    when(taskRepository.findById(anyInt())).thenThrow(
+      new NotFoundException(String.format(
+        "A task with the given ID was not found. [taskId: %s]",
+        99
+      ))
+    );
+
+    Assertions.assertThrows(
+      NotFoundException.class,
+      () -> underTest.findById(99)
+    );
+  }
+
+  @Test
+  void register() {
+  }
+
+  @Test
+  void finish() {
+  }
+
+  @Test
+  void delete() {
+  }
+}
