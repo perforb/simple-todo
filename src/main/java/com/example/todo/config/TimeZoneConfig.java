607f4df3da5639e3837d0b354fb99dba64370bac
perforb <dev.perfumed.garden@gmail.com>
2018-07-18 16:39:14 +0900

    Output time zone when initializeing the application

--- src/main/java/com/example/todo/config/TimeZoneConfig.java
+++ src/main/java/com/example/todo/config/TimeZoneConfig.java
@@ -1,6 +1,8 @@
 package com.example.todo.config;
 
 import com.example.todo.library.datetime.DateTimeProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -11,9 +13,12 @@ import java.time.ZoneId;
 @Configuration
 public class TimeZoneConfig {
 
+  private static final Logger log = LoggerFactory.getLogger(TimeZoneConfig.class);
+
   @Bean
   DateTimeProvider dateTimeProvider(Environment env) {
     String timeZone = env.getProperty("app.time-zone", "UTC");
+    log.info("Initializing the time zone with {}", timeZone);
     ZoneId zoneId = ZoneId.of(timeZone);
     Clock clock = Clock.system(zoneId);
     return new DateTimeProvider(clock);
