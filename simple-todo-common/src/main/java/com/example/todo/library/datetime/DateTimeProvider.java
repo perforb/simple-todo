e54a78aad986525e03ec1dc3e8979f7e156428e3
perforb <dev.perfumed.garden@gmail.com>
2018-11-08 18:28:53 +0900

    Polish

--- simple-todo-common/src/main/java/com/example/todo/library/datetime/DateTimeProvider.java
+++ simple-todo-common/src/main/java/com/example/todo/library/datetime/DateTimeProvider.java
@@ -4,14 +4,12 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Objects;
 
 public class DateTimeProvider {
 
   private final Clock clock;
 
   public DateTimeProvider(Clock clock) {
-    Objects.requireNonNull(clock);
     this.clock = clock;
   }
 
